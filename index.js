02cca153e04e86a48c2453460d505c8de345fc9f
RyosukeSomeya <rx.78.0618@gmail.com>
2021-09-10 09:46:36 +0900

    ejs導入

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// テンプレートエンジンの指定
+app.set("view engine", "ejs");
 
 app.get('/', (req,res) => {
-    res.send('<h1>Express App.</h1>')
+    const data = {
+        items: [
+            {name: "<h1>リンゴ</h1>"},
+            {name: "<h2>バナナ</h2>"},
+            {name: "<h3>スイカ</h3>"}
+        ]
+    };
+    // レンダリングを行う
+    res.render("index.ejs", data);
 });
 
-app.listen(3000, function(){
-    console.log('server running!');
-});
+app.listen(3000);
